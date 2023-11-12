c81b17115ee2c9d228d4e6738cf01f9afc729cd7
Kenneth Elliot <kenneth.elliot@firstmac.com.au>
2023-11-12 20:52:42 +0800

    UI and Font error fixes - Initial commit

--- admin/content_management/front-end/logo.php
+++ admin/content_management/front-end/logo.php
@@ -1,7 +1,7 @@
-<?php if ($_settings->chk_flashdata('success')): ?>
-<script>
-	alert_toast("<?php echo $_settings->flashdata('success') ?>", 'success')
-</script>
+<?php if ($_settings->chk_flashdata('success')) : ?>
+    <script>
+        alert_toast("<?php echo $_settings->flashdata('success') ?>", 'success')
+    </script>
 <?php endif; ?>
 
 <div class="col-lg-12">
@@ -12,13 +12,13 @@
         <div class="card-body">
             <form action="" id="system-frm">
                 <div id="msg" class="form-group"></div>
-               
-                
 
 
 
-              
-               
+
+
+
+
                 <div class="form-group">
                     <label for="" class="control-label">System Logo</label>
                     <div class="custom-file">
@@ -29,25 +29,25 @@
                 <div class="form-group d-flex justify-content-center">
                     <img src="<?php echo validate_image($_settings->info('logo')) ?>" alt="" id="cimg" class="img-fluid img-thumbnail">
                 </div>
-               
-               
 
-                    <label for="sysname" class="control-label">System Name</label>
-                    <textarea name="sysname" id="sysname" cols="1" rows="1" class="form-control summernote">
+
+
+                <label for="sysname" class="control-label">System Name</label>
+                <textarea name="sysname" id="sysname" cols="1" rows="1" class="form-control summernote">
                      <?php echo $_settings->info('sysname')  ?>  
                     </textarea>
 
 
-                    <label for="sys_shortname" class="control-label">System Shortcut Name</label>
-                    <textarea name="sys_shortname" id="sys_shortname" cols="1" rows="1" class="form-control summernote">
+                <label for="sys_shortname" class="control-label">System Shortcut Name</label>
+                <textarea name="sys_shortname" id="sys_shortname" cols="1" rows="1" class="form-control summernote">
                      <?php echo $_settings->info('sys_shortname')  ?>  
                     </textarea>
 
-                    
 
-                 <div>
-               
-               
+
+                <div>
+
+
             </form>
         </div>
         <div class="card-footer">
@@ -62,38 +62,36 @@
 
 
 <script>
-   	function displayImg(input,_this) {
-	    if (input.files && input.files[0]) {
-	        var reader = new FileReader();
-	        reader.onload = function (e) {
-	        	$('#cimg').attr('src', e.target.result);
-	        	_this.siblings('.custom-file-label').html(input.files[0].name)
-	        }
+    function displayImg(input, _this) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $('#cimg').attr('src', e.target.result);
+                _this.siblings('.custom-file-label').html(input.files[0].name)
+            }
 
-	        reader.readAsDataURL(input.files[0]);
-	    }
-	}
-	
-	
-$(document).ready(function(){
-    var summernoteInstance = $('.summernote').summernote({
-        toolbar: [
-            ['style', ['style']],
-            
-            ['fontname', ['fontname']],
-            ['fontsize', ['fontsize']], // Include the font size dropdown
-            ['color', ['color']],
-            
-            
-            ['view', ['undo', 'redo', 'fullscreen', 'codeview', 'help']]
-        ],
-        fontSizes: ['8', '9', '10', '11', '12', '14', '16', '18', '24', '36', '48' , '64', '82', '150'],
-        fontNames: ['Arial', 'Times New Roman', 'Courier New', 'Custom Font', 'Montserrat'], 
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+
+    $(document).ready(function() {
+        var summernoteInstance = $('.summernote').summernote({
+            toolbar: [
+                ['style', ['style']],
+
+                ['fontname', ['fontname']],
+                ['fontsize', ['fontsize']], // Include the font size dropdown
+                ['color', ['color']],
+
+
+                ['view', ['undo', 'redo', 'fullscreen', 'codeview', 'help']]
+            ],
+            fontSizes: ['8', '9', '10', '11', '12', '14', '16', '18', '24', '36', '48', '64', '82', '150'],
+            fontNames: ['Arial', 'Times New Roman', 'Courier New', 'Custom Font', 'Montserrat'],
+        });
+
+
 
     });
-
-   
-   
-});
-
 </script>
--- login.php
+++ login.php
@@ -1,175 +1,48 @@
-<?php  require_once('./config.php');  ?>
+<?php require_once('./config.php');  ?>
 <!DOCTYPE html>
 <html lang="en">
-<?php   require_once('inc/header.php');  ?> 
+<?php require_once('inc/header.php');  ?>
 
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Julius+Sans+One&family=Montserrat:wght@800&family=Poppins:wght@200;200&display=swap">
+<!-- Montserrat Font (800 weight) -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap">
 
-  <!-- Montserrat Font (800 weight) -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap">
-    
-    <!-- Julius Sans One Font -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Julius+Sans+One&display=swap">
-    
-    <!-- Montserrat Font (800 weight) and Poppins Font (200 weight) -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&family=Poppins:wght@200;200&display=swap">
+<!-- Julius Sans One Font -->
+<link href="https://fonts.googleapis.com/css2?family=Julius+Sans+One&display=swap" rel="stylesheet">
 
-<style>
-  body{
-    margin: 0;
-    padding: 0;
-    border: none;
-    outline: 0;
-    list-style: none;
-    text-decoration: none;
-    font-family: 'Poppins', sans-serif;
-    
-    
-}
+<!-- Montserrat Font (800 weight) and Poppins Font (200 weight) -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&family=Poppins:wght@200;200&display=swap">
+<link rel="stylesheet" href="./css/login.css">
 
-.login-container {
-  display: flex;
-  flex-direction: row;
-}
-
-.left-login {
-  background-color: #FFFFFF;
-  width: 50%;
-  height: 60vh;
- 
-}
-
-.right-login {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #609AC4;
-  width: 50%;
-  height: 100vh;
-}
-
-.right-login img {
-  max-width: 100%;
-  max-height: 100%;
-}
-
-.left-login h1 {
-  display: flex;
-  justify-content: center;
-  margin: 3%;
-  margin-top: 8%;
-  font-family: 'Julius Sans One', sans-serif;
-  font-size: 40px;
-  font-weight: 100;
-}
-
-
-
-#clogin-frm{
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    width: 100%;
-    align-items: center;   
-   
-   
-    
-}
-
-.login-textfield-container {
-    display: flex;
-    
-    flex-direction: column; 
-    margin-top: 15%;  
-    width: 75%;
-  }
-  
-.login-textfield-container input {
-    padding: 2%;
-    border: solid 1px #609AC4; 
-    border-radius: 3px;
-    width: 100%;
-    height: 20px;  
-
-}
-
-label{
-    font-weight: normal;
-}
-
-
-.label-password-login {
-  margin-top: 30px;
-}
-
-.back_to_shop a{
-  text-align: left;
-  font-size: 12px;
-  text-decoration: none;
-}
-
-#login-btn,
-#no-account{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-}
-
-#no-account a {
-  text-decoration: none;
-}
-#login-btn button{
-    
-    margin-top: 8%;
-    background-color: #B6DFF4;
-    width: 30%;
-    color: black;
-    font-size: 15px;
-    padding: 1%;
-    border-radius: 20px;
-    cursor: pointer;
-    border: none;
-   
-}
-
-
-#no-account {
-  font-size: 12px;
-  margin-top: 2%;
-}
-
-</style>
 <body>
   <script>
     start_loader();
   </script>
 
-<?php $page = isset($_GET['page']) ? $_GET['page'] : 'home';  ?>
-    <?php if($_settings->chk_flashdata('success')): ?>
-      <script>
-        alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
-      </script>
-    <?php endif;?>
+  <?php $page = isset($_GET['page']) ? $_GET['page'] : 'home';  ?>
+  <?php if ($_settings->chk_flashdata('success')) : ?>
+    <script>
+      alert_toast("<?php echo $_settings->flashdata('success') ?>", 'success')
+    </script>
+  <?php endif; ?>
 
   <div class="login-container">
     <div class="left-login">
       <h1>FIND YOUR PERFECT FIT</h1>
-     
-        <form id="clogin-frm" action="" method="post">
-          <div class="login-textfield-container">
+
+      <form id="clogin-frm" action="" method="post">
+        <div class="login-textfield-container">
 
           <div class="text-field-login">
-          <div class="input-area">
-            <label class="label" for="email">Email</label>
-            <input type="email" name="email" autofocus placeholder="Enter your email" required>
+            <div class="input-area">
+              <label class="label" for="email">Email</label>
+              <input type="email" name="email" autofocus placeholder="Enter your email" required>
             </div>
           </div>
 
           <div class="text-field-login label-password-login">
-          <div class="input-area">
-            <label class="label" for="password">Password</label>
-            <input type="password" name="password" placeholder="Enter your password" required>
+            <div class="input-area">
+              <label class="label" for="password">Password</label>
+              <input type="password" name="password" placeholder="Enter your password" required>
             </div>
           </div>
           <div class="back_to_shop">
@@ -178,12 +51,12 @@ label{
           <div id="login-btn">
             <button type="submit">Log in</button>
           </div>
-          <div id="no-account">No account? 
-            <a href="<?php echo base_url.'register.php' ?>"> Signup here.</a>
+          <div id="no-account">No account?
+            <a href="<?php echo base_url . 'register.php' ?>"> Signup here.</a>
           </div>
-          </div>
-        </form>
-     
+        </div>
+      </form>
+
     </div>
     <div class="right-login">
       <img src="<?= validate_image($_settings->info('logo')) ?>" alt="System Logo">
@@ -192,9 +65,10 @@ label{
   <script src="plugins/jquery/jquery.min.js"></script>
   <script src="dist/js/adminlte.min.js"></script>
   <script>
-    $(document).ready(function () {
+    $(document).ready(function() {
       end_loader();
     });
   </script>
 </body>
+
 </html>

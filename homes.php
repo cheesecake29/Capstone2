62be26b1040580aa445260d6666b742a882fbe79
Alora Francheska del Prado <francheska@webfirmament.com>
2023-11-23 20:03:50 +0800

    initial version best seller

--- homes.php
+++ homes.php
@@ -13,6 +13,53 @@
 
 
 <body class="Homepage">
+    <section class="py-5">
+        <div class="containerrr">
+            <h1 class="new-arrivals">Best Seller</h1>
+            <div class="row row-cols-sm-1 row-cols-md-2 row-cols-xl-4">
+                <?php
+                $products = $conn->query("SELECT p.*, b.name AS brand, c.category, COUNT(o.product_id) AS order_count
+                    FROM product_list p
+                    INNER JOIN brand_list b ON p.brand_id = b.id
+                    INNER JOIN categories c ON p.category_id = c.id
+                    INNER JOIN order_items o ON o.product_id = p.id
+                    WHERE p.delete_flag = 0 
+                    AND p.status = 1 
+                    GROUP BY p.id
+                    ORDER BY order_count DESC
+                    LIMIT 1;");
+                // Counter variable to keep track of displayed products
+                $counter = 0;
+                while ($row = $products->fetch_assoc()) :
+                    // Increment the counter
+                    $counter++;
+                    // Display your product information here
+                ?>
+                    <a class="product-container1" href="./?p=products/view_product&id=<?= $row['id'] ?>" style="width: 250px;">
+                        <div class="card">
+                            <div class="product-img-holder">
+                                <img src="<?= validate_image($row['image_path']) ?>" alt="Product Image" class="img-top" style="width: 100%; height: 250px; display: flex; justify-content: center; align-items: center;" />
+                            </div>
+                            <div class="card-body border-top">
+                                <div class="name card-title my-0">
+                                    <b><?= $row['name'] ?></b>
+                                </div>
+                                <p class="price">₱<?= strip_tags(html_entity_decode($row['price'])) ?>
+                                    <span class="fas fa-tag"></span>
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                <?php
+                    // Check if we have displayed 4 products, and break out of the loop if true
+                    if ($counter >= 4) {
+                        break;
+                    }
+                endwhile;
+                ?>
+            </div>
+        </div>
+    </section>
     <header class="" id="main-header">
         <div class="home-container d-flex flex-column justify-content-center h-100 px-5">
             <h1><?php echo $_settings->info('homename1') ?></h1>
